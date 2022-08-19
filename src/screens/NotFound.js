2920fdccb9aae51749b7b585c64fd97d8f0b9897
MoNakouzi <mo.nakouzi03@gmail.com>
2022-08-18 20:54:01 -0400

    Completed recipe website

---
+++ src/screens/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+    return (
+        <div class='landing-title'>
+            <h1>404 Error</h1>
+            <p1 class='simple-p1'>Page Not Found</p1>
+        </div>
+    )
+}
+
+export default NotFound
